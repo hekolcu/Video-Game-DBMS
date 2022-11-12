c978cfa942292148784e0fa247af5dbd0aabe15a
hekolcu <hekolcu@gmail.com>
2022-11-12 21:14:41 +0000

    Added control buttons
    made code cleaner and added control buttons

--- main.py
+++ main.py
@@ -6,27 +6,86 @@ class FirstFrame:
     def __init__(self, master: tk.Tk, games):
         self.games = games
         self.master = master
-        self.mainframe = tk.Frame(self.master, bg="red")
+        self.mainframe = tk.Frame(
+            self.master,
+            bg="red"
+        )
         self.mainframe.grid()
 
-        self.title = tk.Label(self.mainframe, text="Video Games", font=("Courier", 20, "bold"), bg="red")
-        self.title.grid(row=0, column=0, columnspan=3)
+        self.title = tk.Label(
+            self.mainframe,
+            text="Video Games",
+            font=("Courier", 20, "bold"),
+            bg="red"
+        )
+        self.title.grid(
+            row=0,
+            column=0,
+            columnspan=3
+        )
 
-        self.button = tk.Button(self.mainframe, text="Switch", bg="darkred", command=self.switch)
-        self.button.grid(row=3, column=3)
+        self.button = tk.Button(
+            self.mainframe,
+            text="Switch",
+            bg="darkred",
+            command=self.switch
+        )
+        self.button.grid(
+            row=3,
+            column=3
+        )
 
-        self.info_frame = tk.Frame(self.master, bg="yellow")
-        self.info_frame.grid(row=1, column=0, columnspan=3)
+        self.info_frame = tk.Frame(
+            self.master,
+            bg="yellow"
+        )
+        self.info_frame.grid(
+            row=1,
+            column=0,
+            columnspan=3
+        )
 
-        self.next_button = tk.Button(self.mainframe, text=">", bg="blue", command=self.next_button_action)
-        self.next_button
+        self.next_button = tk.Button(
+            self.mainframe,
+            text=">",
+            bg="blue",
+            command=self.next_button_action
+        )
+        self.next_button.grid(
+            row=2,
+            column=3
+        )
+
+        self.previous_button = tk.Button(
+            self.mainframe,
+            text="<",
+            bg="blue",
+            command=self.previous_button_action
+        )
+        self.previous_button.grid(
+            row=2,
+            column=0
+        )
+
+        self.more_detail_button = tk.Button(
+            self.mainframe,
+            text="Show",
+            bg="green",
+            command=self.more_detail_button_action
+        )
 
     def switch(self):
         self.mainframe.destroy()
         SecondFrame(self.master, self.games)
 
     def next_button_action(self):
-        print("next")
+        print(self, "next")  # yet to be added
+
+    def previous_button_action(self):
+        print(self, "prev")  # yet to be added
+
+    def more_detail_button_action(self):
+        print(self, "show")  # yet to be added
 
 
 class SecondFrame:
